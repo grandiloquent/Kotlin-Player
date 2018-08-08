ca6544f5c1efa5adf8faebeb38df36bdfc16ba8a
grandiloquent <1785907865@qq.com>
2018-08-08 13:43:15 +0800

    changed

--- app/src/main/java/psycho/euphoria/player/MainActivity.kt
+++ app/src/main/java/psycho/euphoria/player/MainActivity.kt
@@ -5,14 +5,9 @@ import android.os.Bundle
 
 class MainActivity : Activity() {
 
-    private lateinit var mTimeBar: TimeBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mTimeBar = findViewById(R.id.time_bar)
-        mTimeBar.duration = 100
-        mTimeBar.position = 50
-
     }
 }
